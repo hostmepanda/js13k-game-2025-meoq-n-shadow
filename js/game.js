3702ebfcdf7d0f739b400e7ca5c1878d407254d0
hostmepanda <noah.vite@gmail.com>
2025-08-14 21:25:26 +0300

    Refactor GameLoop to use modular update and render methods
    Replaced inline update and render functions with imported methods to improve code modularity and maintainability. Added `gameLoopUpdateMethod` and `gameLoopRenderMethod` in separate helper files for cleaner structure.

--- js/game.js
+++ js/game.js
@@ -1,12 +1,15 @@
 // TODO: import from kontra.min.mjs to reduce size before bundling for prod
 import { init, Sprite, GameLoop } from './engine/kontra.mjs'
 
+import {gameLoopUpdateMethod} from './gameHelpers/update'
+import {gameLoopRenderMethod} from './gameHelpers/render'
+
 const { canvas, context } = init();
 
 (() => {
   new GameLoop({
-    update: function() {},
-    render: function() {}
+    update: gameLoopUpdateMethod,
+    render: gameLoopRenderMethod,
   })
   .start();
 })()
