edfc15637512049008a31f85afc938bdefc336c4
hostmepanda <noah.vite@gmail.com>
2025-08-24 23:50:52 +0300

    Add game over and victory screens rendering logic
    Introduced `renderGameOver`, `renderVictoryBlack`, and `renderVictoryWhite` functions to display respective game-ending screens. Updated the `gameLoopRenderMethod` to call these methods based on the game state, enhancing user feedback and overall game flow.

--- js/gameHelpers/render.js
+++ js/gameHelpers/render.js
@@ -1,6 +1,9 @@
 import {GAME_STATE} from '../states/game'
 import {renderMainMenu} from '../menus/main'
 import {levelRender} from '../levels/levelHelpers'
+import {renderGameOver} from '../menus/gameOver'
+import {renderVictoryBlack} from '../menus/victroryBlack'
+import {renderVictoryWhite} from '../menus/victoryWhite'
 
 export function gameLoopRenderMethod(gameObjects, { GameState, PlayerState }, canvas, context) {
 
@@ -13,6 +16,15 @@ export function gameLoopRenderMethod(gameObjects, { GameState, PlayerState }, ca
     case GAME_STATE.LEVEL3:
     case GAME_STATE.LEVEL4:
       levelRender(GameState.currentState)(gameObjects, {GameState, PlayerState}, { canvas, context })
+      break
+    case GAME_STATE.GAMEOVER:
+      renderGameOver(canvas, context)
+      break
+    case GAME_STATE.VICTORYBLACK:
+      renderVictoryWhite(canvas, context)
+      break
+    case GAME_STATE.VICTORYWHITE:
+      renderVictoryBlack(canvas, context)
       break
   }
 }
---
+++ js/menus/gameOver.js
@@ -0,0 +1,10 @@
+export function renderGameOver(canvas, context) {
+  context.fillStyle = 'black';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = 'white';
+  context.font = '30px Arial';
+  context.fillText('JS13K 2025: Meow & Shadow', canvas.width / 2 - 175, canvas.height / 2 - 30);
+  context.fillText('Game Over :(', canvas.width / 2 - 200, canvas.height / 2 + 30);
+  context.fillText('Hit space to start over', canvas.width / 2 - 200, canvas.height / 2 + 60);
+}
